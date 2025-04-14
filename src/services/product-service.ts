4e57224cbca89004ef719c591f820510391ad8bb
stalindlcruz <stalindlcruz@gmail.com>
2025-04-14 17:05:39 -0400

    class product-service done

---
+++ src/services/product-service.ts
@@ -0,0 +1,21 @@
+import { Product } from "../models/product";
+
+export class ProductService {
+  private products: Product[] = [];
+
+  async fetchProducts(): Promise<void> {
+    try {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      this.products = data.map(
+        (item: any) => new Product(item.id, item.title, item.price, item.image)
+      );
+    } catch (error) {
+      console.error("Error fetching products", error);
+    }
+  }
+
+  getProducts(): Product[] {
+    return this.products;
+  }
+}
